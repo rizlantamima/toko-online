f3955aa9d2f889a48cc9e7a1101857ecb4e7c707
Rizlan Tamima <rizlan@ojekbro.com>
2022-12-06 19:38:53 +0700

    styling

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        ops: 'Open Sans',
+        rubik: ['Rubik', ...defaultTheme.fontFamily.sans],
       },
     },
   },
